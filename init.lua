37d8c9f9892f8a1049e12fea608f734cf53f8823
Mock <matheus.mtb7@gmail.com>
2025-11-27 00:48:50 -0300

    Minimum lua code to connect on controllers. No input handling yet

--- init.lua
+++ init.lua
@@ -5,14 +5,75 @@ local menu = require("menu")
 local boop = require("boop")
 local configloader = require("configloader")
 
+
+
+
+local handler
+local handlerPanda
+local listener
+local listenerPanda
+
+function onCallback(clientid, data)
+    print(data)
+
+    local str = ""
+    for i,b in pairs(data) do  
+        str = str ..'['..i..']' ..type(b) .."("..tostring(b).."), " 
+    end
+    print(clientid.." BT DATA: "..str)
+end
+
+local prevPacket = nil
+local secondPrev = nil
+function onBluetoothMouseCallback(clientid, data)
+    
+    if data[1] == 156 and data[2] == 64 and data[3] == 236 then  
+        print(clientid, "DOWN")
+    elseif data[1] == 2 and data[2] == 48 and data[3] == 0 then  
+        print(clientid, "UP")
+    elseif data[1] == 255 and data[2] == 23 and data[3] == 128 then  
+        print(clientid, "RIGHT")
+    elseif data[1] == 56 and data[2] == 128 and data[3] == 17 then  
+        print(clientid, "LEFT")
+    elseif data[1] == 1 and data[2] == 0 and data[3] == 0 then
+        if prevPacket and prevPacket[1] == 0 and prevPacket[2] == 0 and prevPacket[3] == 0 then
+            if secondPrev and secondPrev[1] == 0 and secondPrev[2] == 0 and secondPrev[3] == 0 then
+                print(clientid, "MIDDLE") 
+            end
+        end
+    elseif data[1] == 234 and data[2] == 0 and data[3] == nil then
+        print(clientid, "BACK") 
+    end
+    secondPrev = prevPacket
+    prevPacket = data
+end
+
+
+function onConnectPanda(clientid, a,b)
+    handlerPanda:WriteToCharacteristics({0,0,0,1}, clientid, "d4d3fafb-c4c1-c2c3-b4b3-b2b1a4a3a2a1", true)
+end
+
 function onSetup()
 
     dictLoad()
-    setMaximumControls(2)
-    acceptBLETypes("d4d31337-c4c1-c2c3-b4b3-b2b1a4a3a2a1", "afaf", "fafb")
+    
+
     setLogDiscoveredBleDevices(false)
     generic.displaySplashMessage("Starting:\nBLE")
     startBLE()
+    
+    handler = BleServiceHandler("00001812-0000-1000-8000-00805f9b34fb")
+    listener = handler:AddCharacteristics("2a4d")
+    listener:SetSubscribeCallback(onBluetoothMouseCallback) 
+
+    handlerPanda = BleServiceHandler("d4d31337-c4c1-c2c3-b4b3-b2b1a4a3a2a1")
+    handlerPanda:SetOnConnectCallback(onConnectPanda)
+    listenerPanda = handlerPanda:AddCharacteristics("d4d3afaf-c4c1-c2c3-b4b3-b2b1a4a3a2a1")
+    listenerPanda:SetSubscribeCallback(onCallback)
+    
+    startBLERadio()
+
+
     local seed = tonumber(dictGet("random_seed")) or millis()
     seed = seed + millis()
     math.randomseed(seed)
